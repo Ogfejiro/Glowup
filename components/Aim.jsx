2191f391a3484257e4132b3fcc2906540f20b528
Ogwara Precious <ogwaraprecious8@gmail.com>
2024-12-12 00:59:25 -0800

    finished

--- components/Aim.jsx
+++ components/Aim.jsx
@@ -1,51 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 const Aim = () => {
   return (
     <div>
-      <div className='text-center pt-12 pb-12'>
-        <h1 className='pb-4 font-bold text-xl' data-aos="fade-left">Aim</h1>
-        <p className='padding-x' data-aos="fade-right">To deliver innovative, reliable, and sustainable electrical solutions that exceed customer expectations, ensuring safety, efficiency, and long-term value in every project.</p>
-        <h2 className='pt-12 font-bold'>Objectives</h2>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-12 pt-10 '>
-            <div className=''>
-            <div className="bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md w-52 h-40" data-aos="fade-left">
-            <div className=" text-gray-100 text-center">
-    <p>Customer Satisfaction</p>
-    <p>Provide high-quality products and professional services to meet and exceed customer needs.</p></div>
-</div>
+      <div className="text-center pt-12 pb-12">
+        <h1 className="pb-4 font-bold text-xl" data-aos="fade-left">Aim</h1>
+        <p className="padding-x" data-aos="fade-right">To deliver innovative, reliable, and sustainable electrical solutions that exceed customer expectations, ensuring safety, efficiency, and long-term value in every project.</p>
+        <h2 className="pt-12 font-bold">Objectives</h2>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-12 pt-10 padding-x">
+          <div className="flex-1 bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md h-48" data-aos="fade-left">
+            <div className="text-gray-100 text-center">
+              <p className="font-bold text-xl pb-3">Customer Satisfaction</p>
+              <p className='pt-3'>Provide high-quality products and professional services to meet and exceed customer needs.</p>
             </div>
-            <div className="bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md w-52 h-40" data-aos="fade-right">
-                <div className='text-gray-100 text-center'>
-                    <p>Safety Compliance</p>
-                    <p>Adhere to the highest safety standards in all installations and maintenance services.</p>
-                </div>
+          </div>
+          <div className="flex-1 bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md h-48" data-aos="fade-right">
+            <div className="text-gray-100 text-center">
+              <p className="font-bold text-xl pb-3">Safety Compliance</p>
+              <p className='pt-3'>Adhere to the highest safety standards in all installations and maintenance services.</p>
             </div>
-            <div className='bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md w-52 h-40' data-aos="fade-left">
-                <div className='text-gray-100 text-center'>
-                    <p>Sustainability</p>
-                    <p>Promote energy-efficient and eco-friendly solutions to minimize environmental impact.</p>
-                </div>
+          </div>
+          <div className="flex-1 bg-gradient-to-r from-gray-500 to-gray-300 p-2 rounded-lg shadow-md h-48" data-aos="fade-left">
+            <div className="text-gray-100 text-center">
+              <p className="font-bold text-xl pb-3">Sustainability</p>
+              <p className='pt-3'>Promote energy-efficient and eco-friendly solutions to minimize environmental impact.</p>
             </div>
-
-            <div className='bg-gradient-to-r from-gray-500 to-white-100 p-2 rounded-lg shadow-md w-52 h-40' data-aos="fade-right">
-                <div className=' text-gray-100 text-center'>
-                    <p>Innovative</p>
-                    <p>Stay updated with the latest technologies to offer cutting-edge electrical solutions.</p>
-                </div>
+          </div>
+          <div className="flex-1 bg-gradient-to-r from-gray-500 to-white-100 p-2 rounded-lg shadow-md h-48" data-aos="fade-right">
+            <div className="text-gray-100 text-center">
+              <p className="font-bold text-xl pb-3">Innovative</p>
+              <p className='pt-3'>Stay updated with the latest technologies to offer cutting-edge electrical solutions.</p>
             </div>
-
-            </div>
-
-            <div className='padding-x pt-10'><p className='padding-x'>
-            By staying true to these aims and objectives, we strive to be a leader in the electrical industry, delivering exceptional value to our clients and contributing positively to the community.</p></div>
-
-
-  
-        
+          </div>
+        </div>
+        <div className="padding-x pt-10">
+          <p className="padding-x">By staying true to these aims and objectives, we strive to be a leader in the electrical industry, delivering exceptional value to our clients and contributing positively to the community.</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Aim
+export default Aim;
