c0d7c114006faedee773c9e2c614733cd5276410
Ogwara Precious <ogwaraprecious8@gmail.com>
2024-12-12 01:15:25 -0800

    me

--- components/Services.jsx
+++ components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
     <section className="bg-hero-pattern bg-cover bg-center bg-fixed opacity-80" id="services">
       <div className="text-center text-white">
         <h1 className="pt-10 pb-10 text-2xl" data-aos="zoom-in">Services</h1>
-        <p className="padding-x" data-aos="fade-in">We offer services tailored to meet your unique needs. Whether you're outfitting your home, upgrading your business infrastructure, or tackling a large-scale project, our team of experienced professionals is here to ensure quality, safety, and reliability.</p>
+        <p className="padding-x" data-aos="fade-in">We offer services tailored to meet your unique needs. Whether you are outfitting your home, upgrading your business infrastructure, or tackling a large-scale project, our team of experienced professionals is here to ensure quality, safety, and reliability.</p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-12 pt-10 pb-10">
           <div className="padding-x md:w-1/2 sm:w-full" data-aos="flip-left">
             <Image
